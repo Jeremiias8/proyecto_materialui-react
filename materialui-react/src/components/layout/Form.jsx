1323ba8b76cc801a37032dd47dd0f7effe8c9697
Jeremías <jeremiias122002@gmail.com>
2023-11-04 13:01:39 +0100

    updating form functions and getting data

--- materialui-react/src/components/layout/Form.jsx
+++ materialui-react/src/components/layout/Form.jsx
@@ -3,19 +3,39 @@ import { Link } from 'react-router-dom';
 
 export const Form = () => {
 
-  const [formulario, setFormulario] = useState("");
+  const [usuario, setUsuario] = useState({});
 
-  const enviarPeticion = (e) => {
+  const enviarPeticion = e => {
     e.preventDefault();
 
-    let nombre = e.target.name.value;
-    let business = e.target.business.value;
+    let datos = e.target;
 
-    if (formulario == true) {
-      setFormulario("name:" + nombre, "business: " + business);
+    let usuario = {
+      nombre: datos.nombre.value,
+      business: datos.business.value,
+      enviar: datos.enviar.value
+    };
 
-      console.log("form enviado correctamente !");
-    }
+    console.log(usuario);
+
+    setUsuario(usuario);
+  }
+
+  const cambiarDatos = e => {
+
+    let name_del_input = e.target.name;
+    let business_del_input = e.target.business;
+
+    setUsuario(estado_previo => {
+      return {
+        ...estado_previo,
+        [name_del_input]: e.target.value,
+        [business_del_input]: e.target.value
+      }
+    });
+
+    console.log(name_del_input, business_del_input);
+
   }
 
   return (
@@ -25,14 +45,25 @@ export const Form = () => {
         <Link to="/inicio" className='see-more'>Ver +</Link>
       </span>
 
+      {
+        usuario.name && usuario.name.length >= 1 && 
+        (
+          <div>
+              <strong>{usuario.nombre}</strong> 
+              <br />
+              es de la empresa: <p><i>{usuario.business}</i></p>
+          </div>
+        )
+      }
+
       <form className='form-ui' onSubmit={enviarPeticion}>
 
-        <input type="text" name='name' placeholder="Nombre:" />
-        <input type="text" name='business' placeholder="Empresa:" />
-        <textarea name="text-field" id="text-field" ></textarea>
+        <input type="text" name='name' onChange={cambiarDatos} placeholder="Nombre:" />
+        <input type="text" name='business' onChange={cambiarDatos}  placeholder="Empresa:" />
+        <textarea name="text-field" onChange={cambiarDatos} id="text-field" ></textarea>
         
         <label htmlFor="ask">Quieres concretar una entrevista ?</label>
-        <input type="submit" value="Enviar petición" />
+        <input type="submit" name='enviar' value="Enviar petición" />
 
       </form>
 
